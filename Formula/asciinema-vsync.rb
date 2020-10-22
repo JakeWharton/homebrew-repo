c656361331a3f04765d4dda0eb935f48b947732c
Jake Wharton <jw@squareup.com>
2020-10-22 11:10:25 -0400

    Add asciinema-vsync

---
+++ Formula/asciinema-vsync.rb
@@ -0,0 +1,16 @@
+class DependencyTreeDiff < Formula
+  desc "Batch the commands inside an Asciinema JSON file to reduce flickering"
+  homepage "https://github.com/JakeWharton/asciinema-vsync"
+  url "https://github.com/JakeWharton/asciinema-vsync/releases/download/1.0.0/asciinema-vsync.jar"
+  version "1.0.0"
+  sha256 "dce0570141755f17be1a73a6c7bd79d2deb2f63c96ce1be9a44af308ea0fa23f"
+
+  bottle :unneeded
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "asciinema-vsync.jar"
+    bin.write_jar_script libexec/"asciinema-vsync.jar", "asciinema-vsync"
+  end
+end
