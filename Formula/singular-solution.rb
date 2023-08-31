da0f55ea42cf220aea66d71cdfcc81049de3b91d
Jake Wharton <jw@squareup.com>
2023-08-31 16:22:07 -0400

    singular-solution 2.0.0

--- Formula/singular-solution.rb
+++ Formula/singular-solution.rb
@@ -1,16 +1,16 @@
 class SingularSolution < Formula
   desc "Keep your Twitter follower count at zero by blocking and then quickly unblocking any new followers"
   homepage "https://github.com/JakeWharton/singular-solution"
-  url "https://github.com/JakeWharton/dodo/releases/download/1.0.0/singular-solution.zip"
-  version "1.0.0"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/JakeWharton/singular-solution/releases/download/2.0.0/singular-solution.zip"
+  version "2.0.0"
+  sha256 "e42262a1e46f120091c1cfd34b6aba1e9ffb05ca51c9d9251f2df7cd88cf2adf"
 
   depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
-    (bin/"dodo").write_env_script libexec/"bin/singular-solution",
+    (bin/"singular-solution").write_env_script libexec/"bin/singular-solution",
       :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
