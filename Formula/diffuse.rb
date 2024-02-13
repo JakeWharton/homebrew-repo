ba4a1bb2891b1c7836a00fffe0675b8460d08199
Jake Wharton <jw@squareup.com>
2024-02-13 13:13:56 -0500

    Diffuse 0.2

--- Formula/diffuse.rb
+++ Formula/diffuse.rb
@@ -1,16 +1,17 @@
 class Diffuse < Formula
   desc "A tool for diffing APKs, AABs, AARs, and JARs"
   homepage "https://github.com/JakeWharton/diffuse"
-  url "https://github.com/JakeWharton/diffuse/releases/download/0.1.0/diffuse-0.1.0-binary.jar"
-  version "0.1.0"
+  url "https://github.com/JakeWharton/diffuse/releases/download/0.2.0/diffuse-0.2.0.zip"
+  version "0.2.0"
   sha256 "60d619373c46a5d06b8126c1d61e0adc18b72f2cbb9245ef920d3387e44b86cf"
 
   depends_on "openjdk"
 
   def install
-  	mv "diffuse-#{version}-binary.jar", "diffuse.jar"
-    libexec.install "diffuse.jar"
-    bin.write_jar_script libexec/"diffuse.jar", "diffuse"
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    (bin/"diffuse").write_env_script libexec/"bin/diffuse",
+      :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
   end
 
   test do
